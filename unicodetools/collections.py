a3b4a764abe221aedee4e65ecdbae2bbc50bfb6c
Geoffrey Poore <gpoore@gmail.com>
2017-01-17 07:49:25 -0600

    working on CodePointMultiRange

--- unicodetools/collections.py
+++ unicodetools/collections.py
@@ -122,7 +122,10 @@ class CodePointRange(object):
         if escapefunc is None:
             def ef(cp):
                 if cp <= 0xFFFF:
-                    return '\\u{0:04X}'.format(cp)
+                    if any(ord(first) <= cp <= ord(last) for first, last in [('0', '9'), ('A', 'Z'), ('a', 'z')]):
+                        return chr(cp)
+                    else:
+                        return '\\u{0:04X}'.format(cp)
                 return '\\U{0:08X}'.format(cp)
             escapefunc = ef
         elif not hasattr(escapefunc, '__call__'):
@@ -219,22 +222,20 @@ class CodePointRange(object):
 
 
 
-def containers_to_codepointranges(*containers, **kwargs):
+def codepoints_to_codepointranges(*containers, **kwargs):
     '''
-    Convert containers of code points into a sequence of CodePointRange
+    Convert containers of code points into a list of CodePointRange
     objects.
     '''
     codepoints = set()
     if len(containers) == 0:
-        raise TypeError('One or more containers (tuples, lists, sets) are required as arguments')
+        raise TypeError('One or more containers are required as arguments')
     unpaired_surrogates = kwargs.pop('unpaired_surrogates', False)
     if unpaired_surrogates not in (True, False):
         raise ValueError('"unpaired_surrogates" must be boolean')
     if kwargs:
         raise TypeError('Unknown keyword argument(s):  {0}'.format(', '.join(str(k) for k in kwargs)))
     for container in containers:
-        if not any(isinstance(container, t) for t in (tuple, list, set)):
-            raise TypeError('Arguments must be instances of tuple, list, or set')
         if all(isinstance(x, int) for x in container):
             for x in container:
                 codepoints.update((x,))
@@ -244,8 +245,6 @@ def containers_to_codepointranges(*containers, **kwargs):
                 # builds or for Python 3.3+, so no additional checking is
                 # needed; `ord()` will raise any necessary errors.
                 x_int = ord(x)
-                if 0xD800 <= x_int <= 0xDFFF and not unpaired_surrogates:
-                    raise ValueError('Individual Unicode surrogates (U+D800 - U+DFFF) are not allowed by default; use "unpaired_surrogates"=True if you must have them')
                 codepoints.update((x_int,))
         else:
             raise TypeError('Arguments must be containers consisting solely of integers or solely of Unicode strings that represent individual code points')
@@ -269,3 +268,155 @@ def containers_to_codepointranges(*containers, **kwargs):
             codepoint_ranges.append(CodePointRange(first, last, unpaired_surrogates=unpaired_surrogates))
             first = None
     return codepoint_ranges
+
+
+
+
+class CodePointMultiRange(object):
+    '''
+    A collection of non-overlapping CodePointRanges.  Methods allow the
+    creation of minimal regular expression patterns.
+    '''
+    def __init__(self, codepoints=None, codepointranges=None):
+        if (codepoints is None and codepointranges is None) or (codepoints is not None and codepointranges is not None):
+            raise TypeError('A single keyword argument ("codepoints" or "codepointranges") is required')
+        if codepoints is not None:
+            ranges_no_overlap = codepoints_to_codepointranges(codepoints)
+        else:
+            ranges = []
+            for x in codepointranges:
+                if isinstance(x, CodePointRange):
+                    ranges.append(x)
+                elif all(isinstance(y, CodePointRange) for y in x):
+                    for y in x:
+                        ranges.append(y)
+                else:
+                    raise TypeError('Keyword argument "codepointranges" must give CodePointRanges or containers of them')
+            if not ranges:
+                raise TypeError('Keyword argument "codepointranges" must give CodePointRanges or containers of them')
+            ranges = sorted(ranges, key=lambda x: x.first)
+            ranges_no_overlap = []
+            ranges_iter = iter(ranges)
+            r = next(ranges_iter)
+            r_next = next(ranges_iter, None)
+            while r_next is not None:
+                if r_next.first in r or r_next.first == r.last + 1:
+                    first = min(r.first, r_next.first)
+                    last = max(r.last, r_next.last)
+                    unpaired_surrogates = any(r.unpaired_surrogates, r_next.unpaired_surrogates)
+                    r = CodePointRange(first, last, unpaired_surrogates=unpaired_surrogates)
+                    r_next = next(ranges_iter, None)
+                else:
+                    ranges_no_overlap.append(r)
+                    r = r_next
+                    r_next = next(ranges_iter, None)
+            ranges_no_overlap.append(r)
+        self.codepointranges = ranges_no_overlap
+
+    def __repr__(self):
+        return '{0}.{1}({3})'.format(self.__module__, type(self).__name__, ', '.join(repr(x) for x in self.codepointranges))
+
+    def __iter__(self):
+        ranges_iter = iter(self.codepointranges)
+        r = next(ranges_iter)
+        current = r.first
+        last = r.last
+        overall_last = self.codepointranges[-1].last
+        while current <= overall_last:
+            yield current
+            current += 1
+            if current > last:
+                r = next(ranges_iter, None)
+                if r is not None:
+                    current = r.first
+                    last = r.last
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)) and self.codepointranges == other.codepointranges:
+            return True
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __contains__(self, value):
+        if not isinstance(value, int):
+            return False
+        return any(value in r for r in self.codepointranges)
+
+
+    def as_generic_re_pattern(self, utf16=False, escapefunc=None):
+        '''
+        Express the range as a generic regular expression pattern.
+        '''
+        if len(self._codepointranges) == 1:
+            pattern = self._codepointranges[0].as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc)
+        else:
+            if utf16:
+                bmp_pattern_parts = []
+                astral_pattern_parts = []
+                for r in self._codepointranges:
+                    if r.last <= 0xFFFF:
+                        if r.first == r.last:
+                            bmp_pattern_parts.append(r.as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc))
+                        else:
+                            bmp_pattern_parts.append(r.as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc)[1:-1])
+                    else:
+                        astral_pattern_parts.append(r.as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc)[1:-1])
+                if astral_pattern_parts:
+                    pattern = '[{0}]|{1}'.format(''.join(bmp_pattern_parts), '|'.join(astral_pattern_parts))
+                else:
+                    pattern = '[{0}]'.format(''.join(bmp_pattern_parts))
+            else:
+                pattern_parts = []
+                for r in self._codepointranges:
+                    if r.first == r.last:
+                        pattern_parts.append(r.as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc))
+                    else:
+                        pattern_parts.append(r.as_generic_re_pattern(utf16=utf16, escapefunc=escapefunc)[1:-1])
+                pattern = '[{0}]'.format(''.join(pattern_parts))
+        return pattern
+
+
+    def as_python_3_3_plus_re_pattern(self):
+        '''
+        Express the range as a regular expression pattern suitable for
+        compiling with `re` under Python 3.3+.
+        '''
+        return self.as_generic_re_pattern()
+
+
+    def as_python_before_3_3_re_pattern(self, **kwargs):
+        '''
+        Express the range as a regular expression pattern suitable for
+        compiling with `re` under Python < 3.3, with the specified build
+        width.
+        '''
+        if 'utf16' not in kwargs:
+            raise TypeError('Keyword argument "utf16" (build width) is required')
+        utf16 = kwargs.pop('utf16')
+        if kwargs:
+            raise TypeError('Invalid keyword argument(s):  {0}'.format(', '.join(str(k) for k in kwargs)))
+        def ef(cp):
+            if any(ord(first) <= cp <= ord(last) for first, last in [('0', '9'), ('A', 'Z'), ('a', 'z')]):
+                return chr(cp)
+            return '\\' + chr(cp)
+        return self.as_generic_re_pattern(utf16=utf16, escapefunc=ef)
+
+
+    def as_current_python_version_re_pattern(self):
+        '''
+        Express the range as a regular expression pattern suitable for
+        compiling under the current version of Python.  This accounts for
+        whether `\\u` and `\\U` escapes are supported (Python 3.3+) as well
+        as wide vs. narrow builds (Python < 3.3).
+        '''
+        if sys.version_info.major == 2 or (sys.version_info.major == 3 and sys.version_info.minor < 3):
+            if sys.maxunicode == 0xFFFF:
+                utf16 = True
+            else:
+                utf16 = False
+            version_pattern = self.as_python_before_3_3_re_pattern(utf16=utf16)
+        else:
+            version_pattern = self.as_python_3_3_plus_re_pattern()
+        return version_pattern
